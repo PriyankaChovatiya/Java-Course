255994e283998a1f44e53a8db1deb5b8df09f368
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-10 15:24:04 +0530

    Intro, Variable and DataType

---
+++ class 2 Variables/finalvar.java
@@ -0,0 +1,9 @@
+ class finalvar {
+    
+    public static void main(String[] args) {
+
+        final int myAge = 22;
+        // myAge  = 25 ;
+        System.out.println("myage:" +myAge);  //This will show you error 
+    }
+}
---
+++ class 2 Variables/variables.java
@@ -0,0 +1,22 @@
+class variables{
+
+public static void main(String arg[]) {
+    
+     String name = "Priyaa";
+     System.out.println("My name is: "+name);
+
+     char myLetter ='A';
+     System.out.println("My Letter is: "+myLetter);
+
+     float myFloatnum = 1020f;
+     System.out.println("My Float Number is: "+myFloatnum);
+
+     boolean myBool = true;
+     System.out.println("My Boolean is: "+myBool);
+
+     int myAge = 22;
+     myAge = 25;
+     System.out.println("My Age is: "+myAge);  //now 25
+
+}
+}
