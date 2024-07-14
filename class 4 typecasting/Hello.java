4f29a6275e12e1fb4d05bd58672bc61150d41f62
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-14 18:32:59 +0530

    Type casting (byte)

---
+++ class 4 typecasting/Hello.java
@@ -0,0 +1,10 @@
+class Hello {
+    public static void main(String[] args) {
+       
+        byte b = 127;     //127 is last byte range 
+        int a = b;       
+
+        System.out.println(a);
+
+    }
+}
