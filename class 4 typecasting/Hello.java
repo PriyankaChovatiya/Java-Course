8cd375683bd093d0e7efd8f87c69f5b18a6ff1e8
priyanka <142677957+PriyankaChovatiya@users.noreply.github.com>
2024-07-15 17:02:57 +0530

    Type casting (Error if byte range more than 127)

--- class 4 typecasting/Hello.java
+++ class 4 typecasting/Hello.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String[] args) {
        
-        byte b = 127;     //127 is last byte range 
+        byte b = 257;     //127 is last byte range 
         int a = b;       
 
         System.out.println(a);
